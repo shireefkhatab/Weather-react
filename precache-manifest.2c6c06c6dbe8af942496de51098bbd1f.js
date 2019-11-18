f7b3b247df348b7f00e9f67f8c620a68977664b1
Reef <sherifkhattab13@gmail.com>
2019-11-18 14:04:28 +0000

    Replace existing files with build folder files

---
+++ precache-manifest.2c6c06c6dbe8af942496de51098bbd1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6708449a884c88f687cd9f0a04caf76f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df8451b593914d2aa88",
+    "url": "/static/css/main.357e9042.chunk.css"
+  },
+  {
+    "revision": "32bea33528385c7e86f9",
+    "url": "/static/js/2.0ae7d99e.chunk.js"
+  },
+  {
+    "revision": "4df8451b593914d2aa88",
+    "url": "/static/js/main.c4a5ec5b.chunk.js"
+  },
+  {
+    "revision": "5000389f7ed9c4979223",
+    "url": "/static/js/runtime~main.107e28ba.js"
+  }
+]);
--- src/components/Header/Header.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Header.css';
-const Header = () =>  <h1>Weather App</h1>
-
-
-export default Header
